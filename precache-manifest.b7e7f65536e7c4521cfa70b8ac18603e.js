2f39b8fd69c51d26e639073031f4fd3ba694ed7b
Akhil <Akhil@AKHIL-PC.com>
2020-06-07 20:55:55 +0400

    Updates

---
+++ precache-manifest.b7e7f65536e7c4521cfa70b8ac18603e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5847f5df6b10385e1cf0071b647325ad",
+    "url": "/CoVid-19Tracker/index.html"
+  },
+  {
+    "revision": "1434c53079113bb18b00",
+    "url": "/CoVid-19Tracker/static/css/main.d4b0fde7.chunk.css"
+  },
+  {
+    "revision": "a3eabd9d4b282c5584e5",
+    "url": "/CoVid-19Tracker/static/js/2.eea6b0cd.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/CoVid-19Tracker/static/js/2.eea6b0cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1434c53079113bb18b00",
+    "url": "/CoVid-19Tracker/static/js/main.294d649b.chunk.js"
+  },
+  {
+    "revision": "4df0842032dc8a45d15c",
+    "url": "/CoVid-19Tracker/static/js/runtime-main.a32cf21c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/CoVid-19Tracker/static/media/image.d7265326.png"
+  }
+]);
